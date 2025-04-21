738af90436f25f8676526ec95927e2825ae5c0c6
Ultra-Code <mega.alpha100@gmail.com>
2025-04-21 12:56:16 +0000

    Zzz into the world of image optimization
    Signed-off-by: Ultra-Code <mega.alpha100@gmail.com>

---
+++ src/main.zig
@@ -0,0 +1,86 @@
+const std = @import("std");
+const builtin = @import("builtin");
+const log = std.log.scoped(.@"examples/basic");
+
+const zzz = @import("zzz");
+const http = zzz.HTTP;
+
+const tardy = zzz.tardy;
+const Tardy = tardy.Tardy(.auto);
+const Runtime = tardy.Runtime;
+const Socket = tardy.Socket;
+
+const Server = http.Server;
+const Router = http.Router;
+const Context = http.Context;
+const Route = http.Route;
+const Respond = http.Respond;
+
+fn baseHandler(ctx: *const Context, _: void) !Respond {
+    return ctx.response.apply(.{
+        .status = .OK,
+        .mime = http.Mime.HTML,
+        .body = "Hello, world!",
+    });
+}
+
+pub fn main() !void {
+    const host: []const u8 = "0.0.0.0";
+    const port: u16 = 9862;
+
+    var debug_allocator: std.heap.DebugAllocator(.{
+        .safety = true,
+        .thread_safe = true,
+    }) = .init;
+    const gpa, const is_debug = gpa: {
+        if (builtin.os.tag == .wasi) break :gpa .{ std.heap.wasm_allocator, false };
+        break :gpa switch (builtin.mode) {
+            .Debug, .ReleaseSafe => .{ debug_allocator.allocator(), true },
+            .ReleaseFast, .ReleaseSmall => .{ std.heap.smp_allocator, false },
+        };
+    };
+    defer if (is_debug) {
+        _ = debug_allocator.deinit();
+    };
+
+    var arena: std.heap.ArenaAllocator = .init(gpa);
+    defer arena.deinit();
+    const arena_alloc = arena.allocator();
+
+    var t = try Tardy.init(arena_alloc, .{
+        .threading = .auto,
+    });
+    defer t.deinit();
+
+    var router = try Router.init(arena_alloc, &.{
+        Route.init("/").get({}, baseHandler).layer(),
+    }, .{});
+    defer router.deinit(arena_alloc);
+
+    // create socket for tardy
+    var socket = try Socket.init(.{ .tcp = .{ .host = host, .port = port } });
+    // TODO: how to use async close
+    defer socket.close_blocking();
+    try socket.bind();
+    try socket.listen(4096);
+
+    const EntryParams = struct {
+        router: *const Router,
+        socket: Socket,
+    };
+
+    try t.entry(
+        EntryParams{ .router = &router, .socket = socket },
+        struct {
+            fn entry(rt: *Runtime, p: EntryParams) !void {
+                var server = Server.init(.{
+                    .stack_size = 1024 * 1024 * 4,
+                    .socket_buffer_bytes = 1024 * 2,
+                    .keepalive_count_max = null,
+                    .connection_count_max = 1024,
+                });
+                try server.serve(rt, p.router, .{ .normal = p.socket });
+            }
+        }.entry,
+    );
+}
